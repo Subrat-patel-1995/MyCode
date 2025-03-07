74e6110342115be70c5cd186c80030eff77334ad
skpatel1995 <Subrata.patel@soffrontcrm.com>
2025-03-07 20:41:44 +0530

    Some practice question.

--- src/main/java/com/propx/business/repo/ScheduleRunRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.scheduling.repository;
-
-import com.example.scheduling.model.ScheduleRun;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRunRepository extends JpaRepository<ScheduleRun, Long> {
-}
-
